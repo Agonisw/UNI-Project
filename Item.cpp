84edab658c1162c7dc5120d5997f3d59ab27efc5
sparky9711 <marc.healyfern2@gmail.com>
2014-11-10 20:36:43 +0000

    Create Item.cpp
    The Item class.

---
+++ Item.cpp
@@ -0,0 +1,51 @@
+
+#include "item.h"
+
+Item::Item(string inDescription, int inWeight, bool inIsFillable, bool inEquipable) {
+	description = inDescription;
+	isFillable = inIsFillable;
+	weightGrams = inWeight;
+	equipable = inEquipable;
+	
+	/**weaponCheck(isWeapon);*/
+}
+
+Item::Item(string inDescription) {
+	description = inDescription;
+}
+
+void Item::setWeight(int inWeightGrams)
+{
+	if (inWeightGrams > 9999 || inWeightGrams < 0)
+		cout << "weight invalid, must be 0<weight<9999";
+	else
+		weightGrams = inWeightGrams;
+}
+
+void Item::setValue(float inValue)
+{
+	if (inValue > 9999 || inValue < 0)
+		cout << "value invalid, must be 0<value<9999";
+	else
+		value = inValue;
+}
+
+/**void Item::setWeaponCheck(int isWeapon)
+{
+if(isWeapon > 0 || isWeapon < 0)
+cout << "Item not a weapon" ;
+else
+cout << "Item is a weapon" ;
+}*/
+
+string Item::getShortDescription()
+{
+	return description;
+}
+
+string Item::getLongDescription()
+{
+	return " item(s), " + description  + ".\n";
+
+}
+
